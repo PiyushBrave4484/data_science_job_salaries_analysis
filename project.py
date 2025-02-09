2be80f64c78aad39bd4e07de71b4d50f02ba4cd4
PiyushBrave4484 <piyushkadam4484@gmail.com>
2025-02-09 17:27:59 +0530

    Update project.py

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ st.set_page_config(
 )
 
 st.title("Data Science Job Salaries Insights")
-menu = ["Home","Python Charts", "Power BI file and dashboard"]
+menu = ["Home", "Python Charts", "Power BI file and dashboard"]
 choice = st.sidebar.selectbox("Select chart type", menu)
 
 if choice == "Home":
@@ -19,19 +19,19 @@ elif choice == "Python Charts":
 
     st.subheader("Salary Growth Over Time")
     image1 = Image.open("line_chart_salary_vs_year.png")
-    st.image(image1, caption='Salary Growth Over Time', width=600)
+    st.image(image1, caption="Salary Growth Over Time", width=600)
 
     st.subheader("Distribution of Experience Level")
     image2 = Image.open("histogram_experience_level.png")
-    st.image(image2, caption='Distribution of Experience Level', width=600)
+    st.image(image2, caption="Distribution of Experience Level", width=600)
 
     st.subheader("Correlation Between Numerical Columns")
     image3 = Image.open("heatmap_for_numerical_columns.png")
-    st.image(image3, caption='Correlation Between Numerical Columns', width=600)
+    st.image(image3, caption="Correlation Between Numerical Columns", width=600)
 
     st.subheader("Box Plot Showing Salary vs. Remote Ratio")
     image4 = Image.open("box_plot_salary_vs_remote_ratio.png")
-    st.image(image4, caption='Box Plot Showing Salary vs. Remote Ratio', width=600)
+    st.image(image4, caption="Box Plot Showing Salary vs. Remote Ratio", width=600)
 
     st.subheader("Other Box Plots")
     image5 = Image.open("box_plots.png")
@@ -40,13 +40,14 @@ elif choice == "Python Charts":
 elif choice == "Power BI file and dashboard":
     st.title("Power BI file and dashboard")
     st.subheader("Sample Dashboard")
-    
-    image6 = Image.open("data_science_BI_dashboard.png")  # Fixed typo
-    st.image(image6, caption='Sample Dashboard', use_column_width=True)
+
+    # Fixing typo in filename
+    image6 = Image.open("data_science_BI_dashbboard.png")
+    st.image(image6, caption="Sample Dashboard", width=600)
 
     st.subheader("Download Power BI pbix File")
     with open("data_science_salaries_project.pbix", "rb") as f:
-        st.download_button(
+        btn = st.download_button(
             label="Download Power BI File",
             data=f,
             file_name="data_science_salaries_project.pbix",
